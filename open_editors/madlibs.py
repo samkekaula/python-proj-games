696d105fbbfdcbff38fddba4f2e3b03af950db0c
samkekaula <samkelisiwe547@gmail.com>
2024-03-01 10:46:56 +0200

    second push

---
+++ open_editors/madlibs.py
@@ -0,0 +1,13 @@
+# youtuber = ""
+
+# print("subscribe to " + youtuber) 
+# print("subscribe to {}".format(youtuber))
+
+adj = input("Adjective: ")
+verb1 = input ("Verb: ")
+verb2 = input ("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"computer programming is so {adj} ! It makes me exited all the time bacause \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
