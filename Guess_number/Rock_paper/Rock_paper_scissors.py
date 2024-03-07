b892a50723a93fe090effa1adb60a83a5053234f
samkekaula <samkelisiwe547@gmail.com>
2024-03-07 10:15:12 +0200

    My game

---
+++ Guess_number/Rock_paper/Rock_paper_scissors.py
@@ -0,0 +1,24 @@
+import random
+
+def play():
+    user = input("what's your choice? 'r' for Rock, 'p' for Paper, 's' for Scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\'s a tie'
+
+
+        # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'You won'
+    
+    return 'You Lost!'
+
+def is_win(player, opponent):
+    #return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent 'r'):
+        return True
+    
+print(play())
